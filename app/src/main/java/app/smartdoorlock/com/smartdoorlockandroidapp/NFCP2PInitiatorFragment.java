29f752ae73dec6752a99a6a4b0cdfd5ec86e4327
l46kok <l46kok@localhost>
2016-10-12 22:22:02 -0600

    NFC P2P Example complete

---
+++ app/src/main/java/app/smartdoorlock/com/smartdoorlockandroidapp/NFCP2PInitiatorFragment.java
@@ -0,0 +1,102 @@
+package app.smartdoorlock.com.smartdoorlockandroidapp;
+
+import android.content.Context;
+import android.net.Uri;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.NfcEvent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import static android.nfc.NdefRecord.createMime;
+
+
+public class NFCP2PInitiatorFragment extends Fragment implements NfcAdapter.CreateNdefMessageCallback, NfcAdapter.OnNdefPushCompleteCallback {
+
+    private IFragmentInteractionListener mListener;
+    private NfcAdapter mNfcAdapter;
+
+    public NFCP2PInitiatorFragment() {
+        // Required empty public constructor
+    }
+
+    public static NFCP2PInitiatorFragment newInstance() {
+        NFCP2PInitiatorFragment fragment = new NFCP2PInitiatorFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_nfcp2p_initiator, container, false);
+
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(getActivity());
+        if (mNfcAdapter == null) {
+            Toast.makeText(getActivity(), "NFC is not available", Toast.LENGTH_LONG).show();
+            return v;
+        }
+        // Register callback
+        mNfcAdapter.setNdefPushMessageCallback(this, getActivity());
+        return v;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction("TEST");
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof IFragmentInteractionListener) {
+            mListener = (IFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    @Override
+    public NdefMessage createNdefMessage(NfcEvent event) {
+        String text = ("Beam me up, Android!\n\n" +
+                "Beam Time: " + System.currentTimeMillis());
+        NdefMessage msg = new NdefMessage(
+                new NdefRecord[] { createMime(
+                        "application/vnd.com.example.android.beam", text.getBytes())
+                        /**
+                         * The Android Application Record (AAR) is commented out. When a device
+                         * receives a push with an AAR in it, the application specified in the AAR
+                         * is guaranteed to run. The AAR overrides the tag dispatch system.
+                         * You can add it back in to guarantee that this
+                         * activity starts when receiving a beamed message. For now, this code
+                         * uses the tag dispatch system.
+                        */
+                        //,NdefRecord.createApplicationRecord("com.example.android.beam")
+                });
+        return msg;
+    }
+
+    @Override
+    public void onNdefPushComplete(NfcEvent event) {
+        Toast.makeText(getActivity(),"NDEF Pushed",Toast.LENGTH_LONG).show();
+    }
+}
---
+++ app/src/main/java/app/smartdoorlock/com/smartdoorlockandroidapp/NFCP2PTargetFragment.java
@@ -0,0 +1,61 @@
+package app.smartdoorlock.com.smartdoorlockandroidapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class NFCP2PTargetFragment extends Fragment {
+
+    private IFragmentInteractionListener mListener;
+
+    public NFCP2PTargetFragment() {
+        // Required empty public constructor
+    }
+
+    public static NFCP2PTargetFragment newInstance() {
+        NFCP2PTargetFragment fragment = new NFCP2PTargetFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_nfcp2p_target, container, false);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction("TEST");
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof IFragmentInteractionListener) {
+            mListener = (IFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+}
