769ae947cf355e9a381ae493281e4086fddb7ea4
l46kok <l46kok@localhost>
2016-11-01 10:02:27 -0600

    WifiSetup - implemented wiring adapter to listview

--- app/src/main/java/app/smartdoorlock/com/smartdoorlockandroidapp/Model/NFCWifiSetupModel.java
+++ app/src/main/java/app/smartdoorlock/com/smartdoorlockandroidapp/Model/NFCWifiSetupModel.java
@@ -1,5 +1,7 @@
 package app.smartdoorlock.com.smartdoorlockandroidapp.Model;
 
+import app.smartdoorlock.com.smartdoorlockandroidapp.Enums.WifiSignalEnum;
+
 /**
  * Created by shuh on 11/1/2016.
  */
@@ -7,5 +9,55 @@ package app.smartdoorlock.com.smartdoorlockandroidapp.Model;
 public class NFCWifiSetupModel {
     private String ssid;
     private String password;
+    private WifiSignalEnum signal;
+    private boolean isEncrypted;
+    private String encryptionType;
 
+    public NFCWifiSetupModel(String ssid, String password, WifiSignalEnum signal, boolean isEncrypted, String encryptionType) {
+        this.ssid = ssid;
+        this.password = password;
+        this.signal = signal;
+        this.isEncrypted = isEncrypted;
+        this.encryptionType = encryptionType;
+    }
+
+    public String getSSID() {
+        return ssid;
+    }
+
+    public void setSsid(String ssid) {
+        this.ssid = ssid;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public WifiSignalEnum getSignal() {
+        return signal;
+    }
+
+    public void setSignal(WifiSignalEnum signal) {
+        this.signal = signal;
+    }
+
+    public String getEncryptionType() {
+        return encryptionType;
+    }
+
+    public void setEncryptionType(String encryptionType) {
+        this.encryptionType = encryptionType;
+    }
+
+    public boolean isEncrypted() {
+        return isEncrypted;
+    }
+
+    public void setEncrypted(boolean encrypted) {
+        isEncrypted = encrypted;
+    }
 }
