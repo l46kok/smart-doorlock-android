4f1d3d192e35b27a53cc4bc8a3ba80c88a82e120
l46kok <2355949+l46kok@users.noreply.github.com>
2016-11-01 09:31:43 -0600

    WifiSetup - Finished very basic layout

---
+++ app/src/main/java/app/smartdoorlock/com/smartdoorlockandroidapp/Model/NFCWifiSetupModel.java
@@ -0,0 +1,11 @@
+package app.smartdoorlock.com.smartdoorlockandroidapp.Model;
+
+/**
+ * Created by shuh on 11/1/2016.
+ */
+
+public class NFCWifiSetupModel {
+    private String ssid;
+    private String password;
+
+}
